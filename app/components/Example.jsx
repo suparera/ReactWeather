3c6f1aed9ca94c1f719689528e855b067d719fd0
suparera <suparera@gmail.com>
2016-11-13 10:28:29 +0700

    Update Copy

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,7 +3,7 @@ var React = require('react')
 var Example = React.createClass({
   render:function(){
     return (
-      <h3>Example Component</h3>
+      <h3>Example Component!</h3>
     );
   }
 });
