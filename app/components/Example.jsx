1e86233a41842fd8186bcae043ff9f92f806c6b1
suparera <suparera@gmail.com>
2016-11-14 09:15:42 +0700

    Added div to example.js

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,7 +3,9 @@ var React = require('react')
 var Example = React.createClass({
   render:function(){
     return (
-      <h3>Example Component!</h3>
+      <div>
+        <h3>Example Component!</h3>
+      </div>
     );
   }
 });
